522818d719d441cf37030e674af54c4d8c96546d
Buzz Tilford <tilford20@gmail.com>
2018-11-02 19:23:03 -0700

    added fast and slow movement

--- js/move.js
+++ js/move.js
@@ -7,22 +7,64 @@ var move = function(game)
 move.prototype = {
 	preload: function() 
 	{
-		// preload
+        game.physics.startSystem(Phaser.Physics.ARCADE);
 	},
 	create: function() 
 	{
 		// create
-		style = { font: "65px Arial", fill: "#ffffff", align: "center" };
-		title = game.add.text(game.width / 2, 50, 'Move!', style);
-		title.anchor.setTo(0.5);
 
-		cat = game.add.sprite(game.width / 2, game.height / 2, 'cat');
-		cat.anchor.setTo(0.5);
+		photographer = game.add.sprite(game.width / 2, game.height / 2, 'cat');
+		photographer.anchor.setTo(0.5);
 		// set cat size to 25%
-		cat.scale.setTo(0.25);
-	},
+		photographer.scale.setTo(0.25);
+        game.physics.enable(photographer, Phaser.Physics.ARCADE);
+
+    },
 	update: function() 
 	{
-		// update
-	},
+        // check if the player is sneaking
+        var control = game.input.keyboard.addKey(Phaser.Keyboard.CONTROL);
+        var crawling = control.isDown;
+        var accelleration = (crawling) ? 5 : 10;
+        var topSpeed = (crawling) ? 40 : 80;
+
+
+        // vertical movement
+        var arrows = game.input.keyboard.createCursorKeys();
+        if (arrows.up.isDown && arrows.down.isUp)
+            {
+                photographer.body.velocity.y-=accelleration;
+            }
+        else if (arrows.down.isDown && arrows.up.isUp)
+            {
+                photographer.body.velocity.y+=accelleration;
+            }
+        else
+            {
+                if (photographer.body.velocity.y > 0)
+                    photographer.body.velocity.y = Math.floor(photographer.body.velocity.y/2);
+                if (photographer.body.velocity.y < 0)
+                    photographer.body.velocity.y = Math.ceil(photographer.body.velocity.y/2);
+            }
+
+        // horizontal movement
+        if (arrows.right.isDown && arrows.left.isUp)
+            {
+                photographer.body.velocity.x+=accelleration;
+            }
+        else if (arrows.left.isDown && arrows.right.isUp)
+            {
+                photographer.body.velocity.x-=accelleration;
+            }
+        else
+            {
+                if (photographer.body.velocity.x > 0)
+                    photographer.body.velocity.x = Math.floor(photographer.body.velocity.x/2);
+                if (photographer.body.velocity.x < 0)
+                    photographer.body.velocity.x = Math.ceil(photographer.body.velocity.x/2);
+            }
+	    photographer.body.velocity.clampX(-topSpeed, topSpeed);
+	    photographer.body.velocity.clampY(-topSpeed, topSpeed);
+
+    },
 };
