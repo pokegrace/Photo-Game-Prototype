66e5902a9087c3e9f5eea4a745bfa14c395bbc61
Grace <grko@ucsc.edu>
2018-11-17 14:33:27 -0800

    change overworld treat size

--- js/prototypes/Treat.js
+++ js/prototypes/Treat.js
@@ -2,6 +2,7 @@ function Treat(x,y){
     Phaser.Sprite.call( this, game, x, y, 'battleUI', 'treat');
 
     this.anchor.setTo(0.5);
+    this.scale.setTo(0.4);
     game.physics.enable(this, Phaser.Physics.ARCADE);
 
     
