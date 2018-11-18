669574c87364f6986bef3ff1f7cd128940bb1f9b
Buzz Tilford <tilford20@gmail.com>
2018-11-18 15:50:16 -0800

    added likes counter to title page

--- js/play.js
+++ js/play.js
@@ -22,12 +22,23 @@ play.prototype = {
 		moveKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
 		tutorialKey = game.input.keyboard.addKey(Phaser.Keyboard.T);
 
+        this.likes = 0;
+        this.likeStyle = {font: "40px Comic Sans MS", fill: 'red', align: 'left', wordWrapWidth: 300, wordWrap: true};
+        this.likeText = game.add.text(75 , 560, "0", this.likeStyle);
+
         game.music[0].play()
         game.music[1].stop()
         game.music[2].stop()
 },
 	update: function() 
 	{
+        if (Math.random() > 0.9 - 0.001 * this.likes)
+        {
+            this.likes++;
+        }
+            console.log(this.likes)
+        this.likeText.setText(Math.floor(this.likes));
+
 		if(battleKey.justPressed())
 		{
             game.state.start('battle');
